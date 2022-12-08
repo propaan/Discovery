90c37cee456e6670c5e754396c0827b2e4556d62
propaan <legrandenzo.pro@gmail.com>
2022-12-08 17:53:35 +0100

    Fix style.css

--- javascript.js
+++ javascript.js
@@ -2,10 +2,6 @@ function goDownArtists() {
     document.getElementById('discover-album').scrollIntoView();
 }
 jQuery(window).load(function() {
- 
-    /*
-        Stop carousel
-    */
     $('.carousel').carousel('pause');
  
 });
